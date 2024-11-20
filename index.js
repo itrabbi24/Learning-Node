f7d457c745e6a5653e397e1060c4e1d0dbd0a3d0
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-20 13:10:25 +0600

    What is module, commonjs vs esm

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-// const Module =  require('./local-1');
+// Local Modules
 
-// console.log(Module.add(1,2));
+// // const Module =  require('./local-1');
+
+// // console.log(Module.add(1,2));
 
 
-const {result, add} =  require('./local-1');
-const {result: result2, add: add2} =  require('./local-2');
+// const {result, add} =  require('./local-1');
+// const {result: result2, add: add2} =  require('./local-2');
 
-console.log(result);
-console.log(result2);
+// console.log(result);
+// console.log(result2);
+
+
+// built-in modules
+
+const path = require('path');
+console.log(path.join(__dirname, 'local-1.js'));
